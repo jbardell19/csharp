88c193260f5b1ff690af15471d67f769017a18af
jbardell19 <catchrb19@gmail.com>
2019-10-16 19:57:05 -0500

    Modified and Commented, finished PigLatin

--- PigLatin/Program.cs
+++ PigLatin/Program.cs
@@ -1,20 +1,23 @@
 ﻿using System;
 using System.Linq;
 
+//LINQ (check out msoft docs for ref)//
+
 namespace PigLatin
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.Write("Enter a word to be converted to Pig Latin: ");
+            Console.Write("Enter a word or sentence to be converted to Pig Latin:");
             string input = Console.ReadLine();
             string input2 = input.ToLower();
             PigLatin(input2);
 
             static void PigLatin(string word)
             {
-                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+                //array//
 
                 int firstVowelPosition = word.IndexOfAny(vowels);
                 if (firstVowelPosition == -1)
@@ -26,14 +29,15 @@ namespace PigLatin
                     char firstLetter = word[0];
                     char lastLetter = word[word.Length - 1];
                     if (vowels.Contains(firstLetter) && vowels.Contains(lastLetter))
+                        //if condition//
                     {
-                        Console.WriteLine(word + "yay");
+                        Console.WriteLine(word + "ay");
                     }
                     else
                     {
                         string firstHalf = word.Substring(0, firstVowelPosition);
                         string secondHalf = word.Substring(firstVowelPosition);
-                        Console.WriteLine(secondHalf + firstHalf + "ay");
+                        Console.WriteLine(secondHalf + firstHalf + "yay");
                     }
 
                 }
