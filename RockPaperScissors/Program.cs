48134bb841045abe4d68515147da49850686ede1
jbardell19 <catchrb19@gmail.com>
2019-10-21 14:43:59 -0500

    Added RockPaperScissors

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Let's play Rock, Paper, Scissors!  ");
+            Console.WriteLine("Please choose rock, paper or scissors: ");
+
+            string Guess = Console.ReadLine();
+            RockPaperScissor(Guess);
+
+        }
+
+        public static void RockPaperScissor(string Guess)
+        {
+           
+
+            }
+        }
+    }
+}
