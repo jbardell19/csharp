b65258c3745f33781ec1cfaef937cda13093f325
jbardell19 <catchrb19@gmail.com>
2019-10-28 18:50:21 -0500

    Starting MasterMind

---
+++ masterMind/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace masterMind
+{
+    class Program
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Mastermind!");
+    }
+    {
+        static string[] secret = new string[2];
+        static string[] colorArray = new string[] { "Red", "Yellow", "Blue" };
+        static bool gameOver = false;
+        static void Main(string[] args)
+        {
+
+    }
+}
