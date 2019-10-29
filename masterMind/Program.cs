ee4b9a645b3ad87aa81e51be4792c7d937e3cba6
jbardell19 <catchrb19@gmail.com>
2019-10-29 15:29:26 -0500

    masterMind save..not working

--- masterMind/Program.cs
+++ masterMind/Program.cs
@@ -1,18 +1,84 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace masterMind
 {
     class Program
-    static void Main(string[] args)
     {
-        Console.WriteLine("Welcome to Mastermind!");
-    }
-    {
-        static string[] secret = new string[2];
-        static string[] colorArray = new string[] { "Red", "Yellow", "Blue" };
-        static bool gameOver = false;
+        static bool isRunning = true;
         static void Main(string[] args)
         {
+            Random rnd = new Random();
+            int colorRandom1 = rnd.Next(1, 4);
+            int colorRandom2 = rnd.Next(1, 4);
+            do
+            {
+                Console.WriteLine(" I am thinking of 2 random colors");
+                Console.WriteLine("your color choices are red, yellow, & blue");
+                Console.WriteLine("Enter your first color");
+                string colorChoice1 = Console.ReadLine().ToLower();
+                Console.WriteLine("enter your next color guess");
+                string colorChoice2 = Console.ReadLine().ToLower();
+                int colorA = 0;
+                int colorB = 0;
+                if (colorChoice1 == "red")
+                {
+                    colorA = 1;
+                }
+                else if (colorChoice1 == "yellow")
+                {
+                    colorA = 2;
+                }
+                else if (colorChoice1 == "blue")
+                {
+                    colorA = 3;
+                }
 
+                if (colorChoice2 == "red")
+                {
+                    colorB = 1;
+                }
+                else if (colorChoice2 == "yellow")
+                {
+                    colorB = 2;
+                }
+                else if (colorChoice2 == "blue")
+                {
+                    colorB = 3;
+                }
+                Console.WriteLine(colorA);
+                Console.WriteLine(colorB);
+                Console.WriteLine(colorRandom1);
+                Console.WriteLine(colorRandom2);
+            } while (isRunning);
+            Console.ReadKey();
+        }
+        public static void getClue(int colorRandom1, int colorRandom2, int colorA, int colorB)
+        {
+            if (colorRandom1 == colorA && colorRandom2 == colorB)
+            {
+                Console.WriteLine("you won!");
+                isRunning = false;
+            }
+            else if (colorRandom1 != colorA && colorRandom2 != colorB)
+            {
+                Console.WriteLine("clue 0-0");
+            }
+            else if (colorRandom1 == colorB || colorRandom2 == colorA)
+            {
+                Console.WriteLine("clue 1-0");
+            }
+            else if (colorRandom1 == colorA && colorRandom2 != colorB || colorRandom1 != colorA && colorRandom2 == colorB)
+            {
+                Console.WriteLine("clue 1-1");
+            }
+            else if (colorRandom1 == colorB && colorRandom1 == colorA)
+            {
+                Console.WriteLine("clue 2-0");
+            }
+        }
     }
 }
