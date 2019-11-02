6f85ea1d4ca160c1f38859f464002971465a67bc
jbardell19 <catchrb19@gmail.com>
2019-11-02 14:59:12 -0500

    Finished GradeBook

---
+++ GradeBook/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Gradebook
+{
+	class MainClass
+	{
+		static string userChoice;
+		public static void Main(string[] args)
+		{
+			Dictionary<string, string> Gradebook = new Dictionary<string, string>();
+			Console.WriteLine("Enter the student's full name or type finished when done");
+			userChoice = Console.ReadLine().ToLower();
+			while (userChoice != "finished")
+			{
+				Console.WriteLine("Enter each assignment number grade separated by a space");
+				string studentGrades = Console.ReadLine();
+				Gradebook.Add(userChoice, studentGrades);
+				Console.WriteLine("Enter the student's full name or type finished when done");
+				userChoice = Console.ReadLine().ToLower();
+			}
+			int lowestGrade;
+			int highestGrade;
+			double averageGrade; //var for avg grade
+
+			foreach (string key in Gradebook.Keys)
+			{
+				Console.WriteLine($"Name: {key}");
+				Console.WriteLine($"Grades: {Gradebook[key]}");
+				int[] SingleGrades;
+				SingleGrades = Array.ConvertAll<string, int>(Gradebook[key].Split(), Convert.ToInt32);
+				//hi low avg
+				lowestGrade = SingleGrades.Min();
+				highestGrade = SingleGrades.Max();
+				averageGrade = SingleGrades.Average();
+				Console.WriteLine(key + " ");
+				Console.WriteLine(key + "'s lowest grade is: " + lowestGrade);
+				Console.WriteLine(key + "'s highest grade is: " + highestGrade);
+				Console.WriteLine(key + "'s average grade is: " + averageGrade);
+			}
+		}
+	}
+}
--- masterMind/Program.cs
+++ masterMind/Program.cs
@@ -4,81 +4,134 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace masterMind
-{
-    class Program
-    {
-        static bool isRunning = true;
-        static void Main(string[] args)
-        {
-            Random rnd = new Random();
-            int colorRandom1 = rnd.Next(1, 4);
-            int colorRandom2 = rnd.Next(1, 4);
-            do
-            {
-                Console.WriteLine(" I am thinking of 2 random colors");
-                Console.WriteLine("your color choices are red, yellow, & blue");
-                Console.WriteLine("Enter your first color");
-                string colorChoice1 = Console.ReadLine().ToLower();
-                Console.WriteLine("enter your next color guess");
-                string colorChoice2 = Console.ReadLine().ToLower();
-                int colorA = 0;
-                int colorB = 0;
-                if (colorChoice1 == "red")
-                {
-                    colorA = 1;
-                }
-                else if (colorChoice1 == "yellow")
-                {
-                    colorA = 2;
-                }
-                else if (colorChoice1 == "blue")
-                {
-                    colorA = 3;
-                }
 
-                if (colorChoice2 == "red")
-                {
-                    colorB = 1;
-                }
-                else if (colorChoice2 == "yellow")
-                {
-                    colorB = 2;
-                }
-                else if (colorChoice2 == "blue")
-                {
-                    colorB = 3;
-                }
-                Console.WriteLine(colorA);
-                Console.WriteLine(colorB);
-                Console.WriteLine(colorRandom1);
-                Console.WriteLine(colorRandom2);
-            } while (isRunning);
-            Console.ReadKey();
-        }
-        public static void getClue(int colorRandom1, int colorRandom2, int colorA, int colorB)
-        {
-            if (colorRandom1 == colorA && colorRandom2 == colorB)
-            {
-                Console.WriteLine("you won!");
-                isRunning = false;
-            }
-            else if (colorRandom1 != colorA && colorRandom2 != colorB)
-            {
-                Console.WriteLine("clue 0-0");
-            }
-            else if (colorRandom1 == colorB || colorRandom2 == colorA)
-            {
-                Console.WriteLine("clue 1-0");
-            }
-            else if (colorRandom1 == colorA && colorRandom2 != colorB || colorRandom1 != colorA && colorRandom2 == colorB)
-            {
-                Console.WriteLine("clue 1-1");
-            }
-            else if (colorRandom1 == colorB && colorRandom1 == colorA)
-            {
-                Console.WriteLine("clue 2-0");
-            }
-        }
-    }
+namespace MasterMind_Simple
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string[] colors = { "red", "blue", "green" };
+
+			int length = colors.GetLength(0);
+			Random generator = new Random();
+			// creates a number 0,1 or 2
+			int x = generator.Next(0, 3);
+			int y = generator.Next(0, 3);
+			//int x = 0, y = 0;
+			string userX, userY;
+			bool game = true;
+			int condition = 0;
+			int turns = 0;
+			Console.Write("Mastermind!\n" +
+						  "The computer will choose two colors Red, blue, or green.\n" +
+						  "You must guess the correct colors and the correct placement first, and second.\n" +
+						  "If neither color nor placement are correct the computer will say 0-0\n" +
+						  "If one color is correct but in the wrong place the computer will say 1-0\n" +
+						  "if one color is correct and in the correct position the computer will say 1-1\n" +
+						  "if both colors are correct but in the wrong positions the computer will say 2-0\n" +
+						  "If you guess both correctly and in the correct positions the computer will say 2-2 and you win!\n");
+			while (game)
+			{
+				turns++;
+
+				Console.Write("Please enter your first color: ");
+				userX = Console.ReadLine();
+				userX = userX.ToLower();
+				while (userX != "red" && userX != "blue" && userX != "green")
+				{
+					Console.Write("Invalid input. Please enter red, blue, or green");
+					userX = Console.ReadLine();
+					userX = userX.ToLower();
+				}
+				Console.Write("Please enter your second color: ");
+				userY = Console.ReadLine();
+				userY = userY.ToLower();
+				while (userY != "red" && userY != "blue" && userY != "green")
+				{
+					Console.Write("Invalid input. Please enter red, blue, or green");
+					userY = Console.ReadLine();
+					userY = userY.ToLower();
+				}
+				condition = eval(colors[x], colors[y], userX, userY);
+				switch (condition)
+				{
+					case 0:
+						{
+							Console.WriteLine("0-0");
+							break;
+						}
+					case 1:
+						{
+							Console.WriteLine("1-0");
+							break;
+						}
+					case 2:
+						{
+							Console.WriteLine("1-1");
+							break;
+						}
+					case 3:
+						{
+							Console.WriteLine("0-2");
+							break;
+						}
+					case 4:
+						{
+							Console.WriteLine("2-2");
+							Console.WriteLine("You won!!);
+							game = false;
+							break;
+						}
+
+				}
+
+			}
+			Console.ReadLine();
+
+		}
+
+
+		static int eval(string x, string y, string userX, string userY)
+		{
+			if ((userX != x && userX != y) && (userY != x && userY != y))
+			{
+				return 0;
+			}
+			else if (userX == y && userY == x)
+			{
+				if (userX == x && userY == y) { return 4; }
+				else return 3;
+			}
+			else if (userX == x && userY != y)
+			{
+				return 2;
+			}
+			else if (userX != x && userY == y)
+			{
+				return 2;
+			}
+			else if (userX == y)
+			{
+				return 1;
+			}
+			else if (userY == x)
+			{
+				return 1;
+			}
+			else if (userX == x && userY != y)
+			{
+				return 2;
+			}
+			else if (userX != x && userY == y)
+			{
+				return 2;
+			}
+			else if (userX == x && userY == y)
+			{
+				return 4;
+			}
+			return 0;
+		}
+	}
 }
