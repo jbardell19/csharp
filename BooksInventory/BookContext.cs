e00237ad29685dec6e87ed939b78b440f03002a9
jbardell19 <catchrb19@gmail.com>
2019-12-01 14:52:12 -0600

    Added version 1 BooksInventory

---
+++ BooksInventory/BookContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Books_Inventory
+{
+	class BookContext : DbContext
+	{
+		public DbSet<Book> books { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			DirectoryInfo ExeDirectory = new DirectoryInfo(AppContext.BaseDirectory);
+			DirectoryInfo ProjectBase = ExeDirectory.Parent.Parent.Parent;
+			string DatabaseFile = Path.Combine(ProjectBase.FullName, "books.db");
+			optionsBuilder.UseSqlite("Data Source=" + DatabaseFile);
+		}
+	}
+}
---
+++ BooksInventory/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Books_Inventory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BookContext context = new BookContext();
+            context.Database.EnsureCreated();
+            addBook(context);
+            printInventory(context);
+            IEnumerable<Book> book = context.books.OrderBy(x => x.Title);
+            foreach (Book b in book)
+            {
+                Console.WriteLine($"{b.ID} - {b.Title} by {b.Author}");
+            }
+            IEnumerable<Book> authbook = context.books.OrderBy(x => x.Author);
+            foreach (Book b in authbook)
+            {
+                Console.WriteLine($"{b.ID} - {b.Title} by {b.Author}");
+            }
+            Console.WriteLine("Do you want to delete a book? Y/N");
+            string delete = Console.ReadLine().ToLower();
+        }
+
+        #region Input
+        public static void GetInput(ref string title, ref string author)
+        {
+            Console.WriteLine("Please enter a book Title: ");
+            title = Console.ReadLine();
+            Console.WriteLine("Please enter the titles Author: ");
+            author = Console.ReadLine();
+        }
+        #endregion
+
+        #region Add
+        private static void addBook(BookContext context)
+        {
+            Console.WriteLine("Would you like to add a book to your inventory? Y/N");
+            string input = Console.ReadLine().ToLower();
+            while (input != "n")
+            {
+                string title = "";
+                string author = "";
+                GetInput(ref title, ref author);
+                Book newBook = new Book(title, author);
+                context.books.Add(newBook);
+                context.SaveChanges();
+                Console.WriteLine("Would you like to add a book to your inventory? Y/N");
+                input = Console.ReadLine().ToLower();
+            }
+        }
+        #endregion
+
+        #region Print
+        public static void printInventory(BookContext context)
+        {
+            foreach (Book b in context.books)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"[{b.ID}\n{b.Title}\n{b.Author}]");
+            }
+        }
+        #endregion
+    }
+}
