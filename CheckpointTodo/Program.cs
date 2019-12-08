f4c2a81efc2aa36d61b801de2d7e86eb341b3c90
jbardell19 <catchrb19@gmail.com>
2019-12-07 20:50:18 -0600

    save state added functionality, still crashes

--- CheckpointTodo/Program.cs
+++ CheckpointTodo/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace CheckpointTodo
+
+namespace CheckPoint2_ToDo
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			do
+			{
+				App app1 = new App();
+				app1.CallsMethods();
+			} while (App.EndProgram());
+
+		}
+	}
+
+
+
+
 }
