cafdd5b5bbd803cd9f12cf1ecc46eb6fac396a84
jbardell19 <catchrb19@gmail.com>
2019-12-09 20:47:56 -0600

    Save state need to add db fix

--- CheckpointTodo/ItemContext.cs
+++ CheckpointTodo/ItemContext.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
 using System.IO;
 
 namespace CheckPoint2_ToDo
